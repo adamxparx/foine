d75dfe62e54e7d0edc71dfc4268a90ac73135a68
Adam Cloyd H. Esparcia <adamcloydesparcia@gmail.com>
2025-10-28 12:10:15 +0800

    setup(foine): add packages entity, repository, service, controller, and dto, and add User entity

---
+++ src/main/java/com/example/foine/entity/User.java
@@ -0,0 +1,37 @@
+package com.example.foine.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    public User() {}
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public void setId(Long id) { this.id = id; }
+    public Long getId() { return id; }
+
+    public void setEmail(String email) { this.email = email; }
+    public String getEmail() { return email; }
+    
+    public void setPassword(String password) { this.password = password; }
+    public String getPassword() { return password; }
+}
